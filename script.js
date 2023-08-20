7e78d9babeaa0869b29bd4fcd15b14de279ff61c
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-08-20 11:20:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -282,7 +282,7 @@ const Weather = () => {
 const Tools = () => {
     const getTools = () => {
         return [{
-             icon: "fa-solid fa-cloud-sun",
+             icon: "fa-solid fa-coins",
                 id: 1,
                 image: "https://static.vecteezy.com/system/resources/previews/019/046/869/non_2x/hand-holding-gold-coins-png.png",
                 label: "Goldencoins",
@@ -346,29 +346,12 @@ const Restaurants = () => {
 const Movies = () => {
     const getMovies = () => {
         return [{
-                desc: "A tale of some people watching over a large portion of space.",
+                desc: "",
                 id: 1,
                 icon: "fa-solid fa-galaxy",
-                image: "https://images.unsplash.com/photo-1596727147705-61a532a659bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFydmVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-                title: "Protectors of the Milky Way"
-            }, {
-                desc: "Some people leave their holes to disrupt some things.",
-                id: 2,
-                icon: "fa-solid fa-hat-wizard",
-                image: "https://images.unsplash.com/photo-1535666669445-e8c15cd2e7d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-                title: "Hole People"
-            }, {
-                desc: "A boy with a dent in his head tries to stop a bad guy. And by bad I mean bad at winning.",
-                id: 3,
-                icon: "fa-solid fa-broom-ball",
-                image: "https://images.unsplash.com/photo-1632266484284-a11d9e3a460a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-                title: "Pot of Hair"
-            }, {
-                desc: "A long drawn out story of some people fighting over some space. Cuz there isn't enough of it.",
-                id: 4,
-                icon: "fa-solid fa-starship-freighter",
-                image: "https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3RhciUyMHdhcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-                title: "Area Fights"
+                image: "https://img.apkfab.com/v2/img/9/6/c/96c1e6a09d1c70f48baf2769891e2227.png?h=100&type=webp",
+                title: "Recent logo"
+        }
             }].map((movie) => {
             const styles = {
                 backgroundImage: `url(${movie.image})`
@@ -383,7 +366,7 @@ const Movies = () => {
                     React.createElement("i", { className: movie.icon }))));
         });
     };
-    return (React.createElement(MenuSection, { icon: "fa-solid fa-camera-movie", id: "movies-section", scrollable: true, title: "Popcorn time!" }, getMovies()));
+    return (React.createElement(MenuSection, { icon: "fa-solid fa-camera-movie", id: "movies-section", scrollable: true, title: "Official logo" }, getMovies()));
 };
 const UserStatusButton = (props) => {
     const { userStatus, setUserStatusTo } = React.useContext(AppContext);
