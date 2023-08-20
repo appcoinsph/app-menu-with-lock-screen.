9e77740a270a39ecb6ccc808575c5d5d050b2600
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-08-20 09:09:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -233,38 +233,38 @@ const Weather = () => {
         return [{
                 id: 1,
                 name: "Mon",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }, {
                 id: 2,
                 name: "Tues",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }, {
                 id: 3,
                 name: "Wed",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }, {
                 id: 4,
                 name: "Thurs",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }, {
                 id: 5,
                 name: "Fri",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }, {
                 id: 6,
                 name: "Sat",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }, {
                 id: 7,
                 name: "Sun",
-                temperature: N.rand(60, 80),
-                weather: WeatherType.
+                temperature: N.rand(60, 80)
+                
             }].map((day) => {
             const getIcon = () => {
                 switch (day.weather) {
