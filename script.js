1efabaf9a6130df98b6206399c3547ee128ad6ca
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-08-20 09:40:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 "use strict";
+var UserStatus;
+(function (UserStatus) {
+    UserStatus["LoggedIn"] = "Logged In";
+    UserStatus["LoggingIn"] = "Logging In";
+    UserStatus["LoggedOut"] = "Logged Out";
+    UserStatus["LogInError"] = "Log In Error";
+    UserStatus["VerifyingLogIn"] = "Verifying Log In";
+})(UserStatus || (UserStatus = {}));
 
 var WeatherType;
 (function (WeatherType) {
